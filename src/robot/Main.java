a9c62b31e87e3454fdad217865112a092f473ec9
21416699 <21416699@3888a5da-c516-4a09-b0b5-17a3f298b57d>
2015-11-27 02:33:02 +0000

    Final

--- src/robot/Main.java
+++ src/robot/Main.java
@@ -17,17 +17,6 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        try {
-            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
-            // If Nimbus is not available, you can set the GUI to another look and feel.
-        }
-
         Modele modele = new Modele();
 
         Controleur controleur = new Controleur(modele);
